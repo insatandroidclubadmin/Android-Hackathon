2ef379def3ca1364841069bceb6766413b7d9507
Meriem Selmi <selmimeem@gmail.com>
2014-02-22 12:58:31 +0100

    Added EmergencyCalls

---
+++ src/com/iac/hackathon/manager/EmergencyCallManager.java
@@ -0,0 +1,81 @@
+package com.iac.hackathon.manager;
+
+import java.util.ArrayList;
+
+import com.iac.hackathon.domain.EmergencyCall;
+import com.iac.hackathon.domain.EmergencyCallCategory;
+
+public class EmergencyCallManager {
+
+	static ArrayList<EmergencyCall> emergencyCalls;
+	static ArrayList<EmergencyCallCategory> categories;
+	
+	
+	
+	public EmergencyCallManager() {
+		super();
+		emergencyCalls = new ArrayList<EmergencyCall>();
+		categories = new ArrayList<EmergencyCallCategory>();
+		init();
+	}
+
+
+
+	private void init() {
+		EmergencyCallCategory police = new EmergencyCallCategory("Police", 197,0);
+		EmergencyCallCategory firemen = new EmergencyCallCategory("Firemen", 198,0);
+		EmergencyCallCategory ambulance = new EmergencyCallCategory("Ambulance",190, 0); ///change image
+		
+		EmergencyCall thief = new EmergencyCall("Thief", 0, police);
+		EmergencyCall accident = new EmergencyCall("Accident", 0, police);
+		
+		EmergencyCall fire = new EmergencyCall("Fire", 0, firemen);
+		
+		EmergencyCall complication = new EmergencyCall("Complication", 0, ambulance);
+		EmergencyCall accidentAmbulance = new EmergencyCall("Accident", 0, ambulance);
+		
+		categories.add(police);
+		categories.add(firemen);
+		categories.add(ambulance);
+		
+		emergencyCalls.add(thief);
+		emergencyCalls.add(accident);
+		emergencyCalls.add(fire);
+		emergencyCalls.add(complication);
+		emergencyCalls.add(accidentAmbulance);
+		
+	}
+
+	public ArrayList<EmergencyCall> getEmergencyCalls() {
+		return emergencyCalls;
+	}
+
+
+
+	public ArrayList<EmergencyCallCategory> getCategories() {
+		return categories;
+	}
+
+	public EmergencyCall getEmergencyCallByName(String name) {
+		for (EmergencyCall call : getEmergencyCalls()) {
+			if(name.equals(call.getName())){
+				return call;
+			}
+		}
+		return null;  //exception
+	}
+
+	public EmergencyCallCategory getEmergencyCallCategoryByName(String name) {
+		for (EmergencyCallCategory category : getCategories()) {
+			if(name.equals(category.getName())){
+				return category;
+			}
+		}
+		return null;  //exception
+	}
+	
+	public static void main(String[] args) {
+//		System.out.println(getEmergencyCallByName("Police"));
+	}
+
+}
