1de8dc63709844ce6a5b211a6ee2cdf7a89319bf
Bedis-ElAchèche <elacheche.bedis@gmail.com>
2014-02-22 22:12:42 +0100

    new commit

--- src/com/iac/hackathon/EmergencyCallFragment.java
+++ src/com/iac/hackathon/EmergencyCallFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class EmergencyCallFragment extends Fragment {
 
@@ -23,3 +22,82 @@ public class EmergencyCallFragment extends Fragment {
 		return rootView;
 	}
 }
+
+
+/*
+
+package com.example.test;
+
+import java.util.ArrayList;
+
+import android.app.ExpandableListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ExpandableListView;
+
+public class MainActivity extends ExpandableListActivity{
+
+	private ArrayList<String> parentItems = new ArrayList<String>();
+	private ArrayList<Object> childItems = new ArrayList<Object>();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		// this is not really  necessary as ExpandableListActivity contains an ExpandableList
+		//setContentView(R.layout.main);
+
+		ExpandableListView expandableList = getExpandableListView(); // you can use (ExpandableListView) findViewById(R.id.list)
+
+		expandableList.setDividerHeight(2);
+		expandableList.setGroupIndicator(null);
+		expandableList.setClickable(true);
+
+		setGroupParents();
+		setChildData();
+
+		MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
+
+		adapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+		expandableList.setAdapter(adapter);
+		expandableList.setOnChildClickListener(this);
+	}
+
+	public void setGroupParents() {
+		parentItems.add("Police");
+		parentItems.add("Fireman");
+		parentItems.add("Ambulance");
+	}
+
+	public void setChildData() {
+
+		// Android
+		ArrayList<String> child = new ArrayList<String>();
+		child.add("Core");
+		child.add("Games");
+		childItems.add(child);
+
+		// Core Java
+		child = new ArrayList<String>();
+		child.add("Apache");
+		child.add("Applet");
+		child.add("AspectJ");
+		child.add("Beans");
+		child.add("Crypto");
+		childItems.add(child);
+
+		// Desktop Java
+		child = new ArrayList<String>();
+		child.add("Accessibility");
+		child.add("AWT");
+		child.add("ImageIO");
+		child.add("Print");
+		childItems.add(child);
+
+	}
+
+}
+
+*/
