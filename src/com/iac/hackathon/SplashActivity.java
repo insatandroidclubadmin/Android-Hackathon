90a3814fc3edb602b852410a5b6af0a9c655bcbc
Meriem Selmi <selmimeem@gmail.com>
2014-02-23 03:22:35 +0100

    SplashScreen

--- src/com/iac/hackathon/SplashActivity.java
+++ src/com/iac/hackathon/SplashActivity.java
@@ -4,27 +4,50 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 	private final int SPLASH_DISPLAY_LENGTH = 3000;
-
+	Animation animation; 
+	ImageView logo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		animation = AnimationUtils.loadAnimation(this, R.anim.down_to_up);
+		logo = (ImageView) findViewById(R.id.splash_image);
+		logo.startAnimation(animation);
+		
+		new Handler().postDelayed(new Runnable() {
 
-		/*
-		 * ImageView logo = (ImageView) findViewById(R.drawable.logo); Animation
-		 * fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
-		 * logo.startAnimation(fadeInAnimation);
-		 */
+			@Override
+			public void run() {
+
+				Intent intent = new Intent(SplashActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+
+				SplashActivity.this.finish();
+
+				overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+			}
+		}, SPLASH_DISPLAY_LENGTH);
+
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				SplashActivity.this.finish();
-				Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-				SplashActivity.this.startActivity(mainIntent);
 			}
 		}, SPLASH_DISPLAY_LENGTH);
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		this.finish();
+		super.onBackPressed();
 	}
 }
