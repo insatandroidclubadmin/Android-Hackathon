699625d3d82d294ce211fc9f9adceeaa567144b3
khoubeib tlili <tlili.khoubeib@gmail.com>
2014-02-23 01:45:59 +0100

    Emergency Call fixed

--- src/com/iac/hackathon/EmergencyCallFragment.java
+++ src/com/iac/hackathon/EmergencyCallFragment.java
@@ -1,25 +1,92 @@
 package com.iac.hackathon;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
 
-public class EmergencyCallFragment extends Fragment {
+import com.iac.hackathon.utils.MyExpandableAdapter;
+
+public class EmergencyCallFragment extends Fragment implements OnChildClickListener{
+	
+	
+	
+	private ArrayList<String> parentItems = new ArrayList<String>();
+	private ArrayList<Object> childItems = new ArrayList<Object>();
+	
 
 	public EmergencyCallFragment() {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView( LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_main_emergency_call,
 				container, false);
-//		TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+		
+		ExpandableListView expandableList = (ExpandableListView) rootView.findViewById(R.id.list);
+
+		expandableList.setDividerHeight(2);
+		expandableList.setGroupIndicator(null);
+		expandableList.setClickable(true);
+
+		setGroupParents();
+		setChildData();
+
+		MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
+
+		adapter.setInflater( (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE), getActivity());
+		expandableList.setAdapter(adapter);
+		expandableList.setOnChildClickListener((OnChildClickListener) this);
 		
 		
 		return rootView;
+	}
+	
+	public void setGroupParents() {
+		parentItems.add("Police");
+		parentItems.add("Fireman");
+		parentItems.add("Ambulance");
+	}
+
+	public void setChildData() {
+
+		// Android
+		ArrayList<String> child = new ArrayList<String>();
+		child.add("Core");
+		child.add("Games");
+		childItems.add(child);
+
+		// Core Java
+		child = new ArrayList<String>();
+		child.add("Apache");
+		child.add("Applet");
+		child.add("AspectJ");
+		child.add("Beans");
+		child.add("Crypto");
+		childItems.add(child);
+
+		// Desktop Java
+		child = new ArrayList<String>();
+		child.add("Accessibility");
+		child.add("AWT");
+		child.add("ImageIO");
+		child.add("Print");
+		childItems.add(child);
+
+	}
+
+	@Override
+	public boolean onChildClick(ExpandableListView parent, View v,
+			int groupPosition, int childPosition, long id) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
 
